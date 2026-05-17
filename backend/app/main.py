80f6712a4f701f760dc758a328b193f596941b46
Saad Ahmad <saad415415@gmail.com>
2026-05-17 02:48:39 +0200

    added link https://www.saadahmad.de

--- backend/app/main.py
+++ backend/app/main.py
@@ -7,7 +7,10 @@ app = FastAPI(title="Medical Landmark AI Backend")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+    "http://localhost:3000",
+    "https://www.saadahmad.de",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
